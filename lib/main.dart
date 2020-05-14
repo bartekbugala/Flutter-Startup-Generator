be8aef236c53e8b0ce4b9753a564cc83d0ed4c29
Bartłomiej Bugała <bugala@trui.pl>
2020-05-14 22:37:07 +0200

    Add a Stateful widget modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// StatelessWidget -> Immutable Widget, that does not require state. Their properties can't change - all values are final
 class MyApp extends StatelessWidget {
   // @override -> (annotation) marks an instance member as overriding a superclass member with the same name.
   @override
@@ -14,17 +15,28 @@ class MyApp extends StatelessWidget {
   // BuildContext -> A handle to the location of a widget in the widget tree.
   //                 This class presents a set of methods that can be used from [StatelessWidget.build] methods and from methods on [State] objects.
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     // MaterialApp -> implementation of google Material Design
     return MaterialApp(
         title: 'Starting Flutter Dev',
         // Scaffold -> Implements the basic material design visual layout structure.
         //             This class provides APIs for showing drawers, snack bars, and bottom sheets.
         home: Scaffold(
-            appBar: AppBar(title: Text('Welcome to Flutter')),
+            appBar: AppBar(title: Text('Welcome to Flutter!')),
             body: Center(
-              child: Text(wordPair.asPascalCase),
+              child: RandomWords(),
             )));
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
