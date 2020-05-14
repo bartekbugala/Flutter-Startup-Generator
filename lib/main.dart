d18a0077f6a91718fe7c239d6a9054fba0a009a6
Bartłomiej Bugała <bugala@trui.pl>
2020-05-14 20:38:11 +0200

    Use an external package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 // runApp -> Inflate the given widget and attach it to the screen.
 //           The widget is given constraints during layout that force it to fill the entire screen.
@@ -13,14 +14,17 @@ class MyApp extends StatelessWidget {
   // BuildContext -> A handle to the location of a widget in the widget tree.
   //                 This class presents a set of methods that can be used from [StatelessWidget.build] methods and from methods on [State] objects.
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     // MaterialApp -> implementation of google Material Design
     return MaterialApp(
         title: 'Starting Flutter Dev',
         // Scaffold -> Implements the basic material design visual layout structure.
         //             This class provides APIs for showing drawers, snack bars, and bottom sheets.
         home: Scaffold(
-          appBar: AppBar(title: Text('Welcome to Flutter')),
-          body: Center(child: Text('Hello World')),
-        ));
+            appBar: AppBar(title: Text('Welcome to Flutter')),
+            body: Center(
+              child: Text(wordPair.asPascalCase),
+            )));
   }
 }
